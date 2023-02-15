3be81d448ac894008749108a9116c6d97b5db774
Heloisa <heloisa@desrotulando.com>
2023-02-15 10:17:27 -0300

    Desafio 1

--- lib/constants/desafio1.dart
+++ lib/constants/desafio1.dart
@@ -1,7 +1,5 @@
 import 'package:desafio_justa/constants/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class Desafio1 extends StatefulWidget {
   const Desafio1({super.key});
@@ -10,16 +8,14 @@ class Desafio1 extends StatefulWidget {
   State<Desafio1> createState() => _Desafio1State();
 }
 
-
- class _Desafio1State extends State<Desafio1> {
+class _Desafio1State extends State<Desafio1> {
+  var string1 = TextEditingController();
+  var string2 = TextEditingController();
   String desafio1Resultado = '';
-  var lista1 = '1, 3, 4, 7, 19';
-  var lista2 = '1, 2, 4, 13, 15';
 
-   formatarLista(lista) => lista.split(",").map((n) => n.trim()).toList();
+  formatarLista(lista) => lista.split(",").map((n) => n.trim()).toList();
 
-
-executarDesafio1(lista1, lista2) {
+  executarDesafio1(lista1, lista2) {
     // transformar a string em uma lista de valores separados por vírgula
     // preparar a entrada removendo espaços
     // transformei numa função pra não repetir
@@ -28,8 +24,8 @@ executarDesafio1(lista1, lista2) {
     //criar uma variavel pra armazenar os numeros comuns
     var comum = [];
     // iterar uma lista, sobre a outra
-    lista1.forEach((n) => lista2.forEach((nm) => {n == nm ? comum.add(n) : null}));
-    print(comum);
+    lista1.forEach(
+        (n) => lista2.forEach((nm) => {n == nm ? comum.add(n) : null}));
     // transformar a lista em uma string separada por virgula
     // salvar o resultado para exibir
     setState(() {
@@ -40,16 +36,33 @@ executarDesafio1(lista1, lista2) {
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Container(child: const Text(desafio1)),
-          const Text(descricaoDesafio1),
-          const Text("Escreva uma lista de numeros separadas por virgulas"),
-          //inserir input para lista 1
-          const Text("Escreva outra lista de numeros separadas por virgulas"),
-          //inserir input para lista 2
-          TextButton(
-              onPressed: (() => executarDesafio1(lista1, lista2)),
-              child: Text("Executar desafio 1")),
-          desafio1Resultado == '' ? Container() : Text(desafio1Resultado)
-        ]);
+      const Center(
+          child: Padding(
+        padding: EdgeInsets.only(bottom: 18.0),
+        child: Text(
+          desafio1,
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500),
+        ),
+      )),
+      const Text(descricaoDesafio1),
+      const Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        child: Text(d1descricao2),
+      ),
+      TextField(
+        controller: string1,
+      ),
+      const Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        child: Text(d1descricao3),
+      ),
+      TextField(
+        controller: string2,
+      ),
+      TextButton(
+          onPressed: (() => executarDesafio1(string1.text, string2.text)),
+          child: const Text("Executar desafio 1")),
+      desafio1Resultado == '' ? Container() : Text(desafio1Resultado)
+    ]);
   }
 }
